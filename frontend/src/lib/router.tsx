f1e9e23b1d2447adff24b0c1cd524aa2601463db
Nathaniel Abrante <nathaniel.lacerda@gmail.com>
2026-06-04 17:45:43 -0300

    feat: implement lazy loading for routes using Suspense in router configuration

--- frontend/src/lib/router.tsx
+++ frontend/src/lib/router.tsx
@@ -1,19 +1,34 @@
 import { Layout } from "@/components/ui/Layout";
-import Assentos from "@/pages/Assentos";
-import Busca from "@/pages/Busca";
-import Checkout from "@/pages/Checkout";
-import Consulta from "@/pages/Consulta";
+import { Box, LinearProgress } from "@mui/material";
+import { lazy, Suspense } from "react";
 import { createBrowserRouter } from "react-router-dom";
+
+const Busca = lazy(() => import("@/pages/Busca"));
+const Assentos = lazy(() => import("@/pages/Assentos"));
+const Checkout = lazy(() => import("@/pages/Checkout"));
+const Consulta = lazy(() => import("@/pages/Consulta"));
+
+const lazyRoute = (Component: React.ComponentType) => (
+  <Suspense
+    fallback={
+      <Box sx={{ width: "100%", position: "fixed", top: 0, left: 0, zIndex: 1400 }}>
+        <LinearProgress color="primary" />
+      </Box>
+    }
+  >
+    <Component />
+  </Suspense>
+);
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     children: [
-      { index: true, element: <Busca /> },
-      { path: "viagem/:id/assentos", element: <Assentos /> },
-      { path: "checkout", element: <Checkout /> },
-      { path: "consulta", element: <Consulta /> },
+      { index: true, element: lazyRoute(Busca) },
+      { path: "viagem/:id/assentos", element: lazyRoute(Assentos) },
+      { path: "checkout", element: lazyRoute(Checkout) },
+      { path: "consulta", element: lazyRoute(Consulta) },
     ],
   },
 ]);
